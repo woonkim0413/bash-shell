ab89f21036788d503631a8c536cb383b7f5265c8
Rakyun Kim <dbskim234@naver.com>
2025-05-10 18:02:41 +0900

    etc

--- parsing/utils/seperate_helper.c
+++ parsing/utils/seperate_helper.c
@@ -6,11 +6,37 @@
 /*   By: rakim <fkrdbs234@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 19:32:26 by rakim             #+#    #+#             */
-/*   Updated: 2025/05/10 15:35:14 by rakim            ###   ########.fr       */
+/*   Updated: 2025/05/10 17:51:16 by rakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static	int	count_pipe_not_in_quote(char *line)
+{
+	int		count;
+	int		idx;
+	int		in_single;
+	int		in_double;
+	char	c;
+
+	count = 1;
+	idx = 0;
+	in_single = 0;
+	in_double = 0;
+	while (line[idx])
+	{
+		c = line[idx];
+		if (c == '\'' && !in_double)
+			in_single = !in_single;
+		else if (c == '"' && !in_single)
+			in_double = !in_double;
+		else if (c == '|' && !in_double && !in_single)
+			count++;
+		idx++;
+	}
+	return (count);
+}
 
 void	set_toggle(char c, int *in_single, int *in_double)
 {
@@ -48,32 +74,6 @@ static	void	process_seperate_line(char ***result, char **line, \
 	}
 	(*result)[result_idx] = ft_substr(*line, start, ft_strlen(*line) - start);
 	free(*line);
-}
-
-static	int	count_pipe_not_in_quote(char *line)
-{
-	int		count;
-	int		idx;
-	int		in_single;
-	int		in_double;
-	char	c;
-
-	count = 1;
-	idx = 0;
-	in_single = 0;
-	in_double = 0;
-	while (line[idx])
-	{
-		c = line[idx];
-		if (c == '\'' && !in_double)
-			in_single = !in_single;
-		else if (c == '"' && !in_single)
-			in_double = !in_double;
-		else if (c == '|' && !in_double && !in_single)
-			count++;
-		idx++;
-	}
-	return (count);
 }
 
 char	**extend_env_and_split(char **line, t_object *object)
