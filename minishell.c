d35260a40b03a61134dfc38cc2f4040318dc70e2
Rakyun Kim <dbskim234@naver.com>
2025-05-12 17:32:37 +0900

    seperate main()

--- minishell.c
+++ minishell.c
@@ -6,16 +6,31 @@
 /*   By: rakim <fkrdbs234@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 16:53:47 by rakim             #+#    #+#             */
-/*   Updated: 2025/05/12 18:50:13 by rakim            ###   ########.fr       */
+/*   Updated: 2025/05/12 18:50:30 by rakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static	void	process_minishell(t_object *object, char **line)
+{
+	char	**line_splited_by_pipe;
+
+	whitespace_convert_to_space(line);
+	check_pipe(line, object);
+	check_quotes(line, object);
+	line_splited_by_pipe = extend_env_and_split(line, object);
+	free(*line);
+	parsing(line_splited_by_pipe, object);
+	handle_heardoc(object);
+	clean_up_quote(object->cmd_info);
+	print_all_cmd(object->cmd_info);
+	implement(object);
+}
+
 int	main(int length, char *input[], char *env[])
 {
 	char		*line;
-	char		**line_splited_by_pipe;
 	t_object	object;
 
 	init(length, input, &object, env);
@@ -31,18 +46,7 @@ int	main(int length, char *input[], char *env[])
 		if (*line != '\0')
 			add_history(line);
 		if (*line != '\0' && !is_all_space(line))
-		{
-			whitespace_convert_to_space(&line);
-			check_pipe(&line, &object);
-			check_quotes(&line, &object);
-			line_splited_by_pipe = extend_env_and_split(&line, &object);
-			free(line);
-			parsing(line_splited_by_pipe, &object);
-			handle_heardoc(&object);
-			clean_up_quote(object.cmd_info);
-			print_all_cmd(object.cmd_info);
-			implement(&object);
-		}
+			process_minishell(&object, &line);
 	}
 	return (0);
 }
